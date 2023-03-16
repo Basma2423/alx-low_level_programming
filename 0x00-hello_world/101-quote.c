867eff0de49729858d83257a241f1938c19f8943
Basma2423 <mayarbasma2423@gmail.com>
2023-03-16 13:31:35 -0700

    dfs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include<unistd.h>
-#include<sys/syscall.h>
+#include<unistd.h>
 /**
  *
  * main - main funciton does some stuff
@@ -10,7 +10,6 @@
  */
 int main(void)
 {
-	char msg[] = "and that piece of art is useful\" - Dorpar, 2015-10-19\n";
-	syscall(SYS_write, 1, msg, sizeof(msg));
+	write(2, "and that piece of art is useful\" - Dorpar, 2015-10-19\n", 59);
 	return (1);
 }
