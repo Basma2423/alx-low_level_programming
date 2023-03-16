ed5391cb86e6ae55b628ed31bcb93f50a0580fae
Basma2423 <mayarbasma2423@gmail.com>
2023-03-16 13:28:18 -0700

    Ya Rab

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,16 @@
-    #include <unistd.h> 
-    #include <sys/syscall.h> 
-    int main(void) 
-    { 
-        const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
-        syscall(SYS_write, 1, msg, sizeof(msg)); 
-        return 1; 
-    } 
+#include<unistd.h>
+#include<sys/syscall.h>
+/**
+ *
+ * main - main funciton does some stuff
+ *
+ * Description: print without printf or puts methods
+ *
+ * Return: 1 as mentioned
+ */
+int main(void)
+{
+	char msg[] = "and that piece of art is useful\" - Dorpar, 2015-10-19\n";
+	syscall(SYS_write, 1, msg, sizeof(msg));
+	return (1);
+}
