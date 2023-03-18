e5215605cf7661b1076f7510ab83d3b9cd70ce99
Basma2423 <mayarbasma2423@gmail.com>
2023-03-18 12:36:29 -0700

    Zor

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - this is the main function
+ *
+ * Description: prints single numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 48; a <= 55; a++)
+	{
+		for (b = a + 1; b <= 56; b++)
+		{
+			for (c = b + 1; c <= 57; c++)
+			{
+
+				if (a != b && b != c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != 55 || b != 56 || c != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+
+}
