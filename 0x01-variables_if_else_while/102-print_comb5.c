14e6173183d24a882da9466c3677bb4392b95e94
Basma2423 <mayarbasma2423@gmail.com>
2023-03-18 13:15:19 -0700

    ALLAH

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,11 +16,10 @@ int main(void)
 	{
 		for (b = 48; b <= 56; b++)
 		{
-			for (c = 48; c <= 57; c++)
+			for (c = a; c <= 57; c++)
 			{
-				for(d = 48; d <= 57; d++)
+				for (d = b + 1; d <= 57; d++)
 				{
-
 					if (a != c || b != d)
 					{
 						putchar(a);
@@ -28,11 +27,28 @@ int main(void)
 						putchar(' ');
 						putchar(c);
 						putchar(d);
-						if (a != 57 || b != 56 || c != 57 || d != 57)
+						if (a == 57 && b ==56)
+						{
+							if (c == a && c == d)
+							{
+
+							}
+
+							else
+							{
+							
+								putchar(',');
+	                                                        putchar(' ');
+
+							}
+							
+						}
+						else
 						{
 							putchar(',');
-							putchar(' ');
+                                                        putchar(' ');
 						}
+						
 					}
 				}
 			}
