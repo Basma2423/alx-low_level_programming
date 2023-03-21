9bbf97e175747f82a0236a529aed4b939d94e941
Basma2423 <mayarbasma2423@gmail.com>
2023-03-21 14:39:24 -0700

    L

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *
+ * print_last_digit - pretty obvious
+ *
+ * @num: parameter number
+ *
+ * Description: pretty obvious
+ *
+ * Return: last digit int
+ */
+int print_last_digit(int num)
+{
+	return (num % 10);
+}
