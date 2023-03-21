ac3d1dae76e37add56bcda250c68b379fd6dc4f7
Basma2423 <mayarbasma2423@gmail.com>
2023-03-21 14:35:21 -0700

    ABS

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value
+ *
+ * @a: parameter will be operated on
+ *
+ * Description: wade7 gedan
+ *
+ * Return: abs(num)
+ */
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		a = -a;
+		return (a);
+	}
+	else
+		return (a);
+}
