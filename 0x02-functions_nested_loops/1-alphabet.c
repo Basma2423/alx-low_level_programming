d4db09fb6228f4b2743ae45ae7ffa520d0da64e5
Basma2423 <mayarbasma2423@gmail.com>
2023-03-21 13:32:26 -0700

    ALPJA

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a void function
+ *
+ * Description: prints the alphabet
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+
+	return (0);
+}
