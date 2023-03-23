8b2f76120d5e5d41cbc5fab7b32e7051f33294d8
Basma2423 <mayarbasma2423@gmail.com>
2023-03-23 14:24:03 -0700

    S

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - obvious
+ *
+ * @n: param
+ *
+ * return: void
+ */
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
