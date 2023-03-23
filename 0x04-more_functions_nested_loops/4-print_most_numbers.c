15a2c453ae7b6b5c40973b18fa2f2da3b248170b
Basma2423 <mayarbasma2423@gmail.com>
2023-03-23 13:53:18 -0700

    S

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - nothing useful
+ *
+ * return: void
+ */
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i < '9'; i++)
+	{
+		if (i == '4' || i == '2')
+			continue;
+		_putchar(i);
+	}
+	_putchar('\n');
+}
