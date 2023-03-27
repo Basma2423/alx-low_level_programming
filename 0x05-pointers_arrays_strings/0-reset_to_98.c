246caa139dd646e04b1b4b6903ab89133cc5cf0c
Basma2423 <mayarbasma2423@gmail.com>
2023-03-27 12:58:48 -0700

    SFA

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98
+ * reset_to_98 - updates the value n is pointing to to 98
  *
- * @n - pointer to an int
+ * @n: pointer to an int
  *
  * Return: 0 as usual
  */
