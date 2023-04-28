83a23eeef209048fd385ddc602e58c26d50ca7f3
Basma2423 <mayarbasma2423@gmail.com>
2023-04-28 09:29:41 -0700

    g

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void myStartupFun (void) __attribute__ ((constructor));
+
+/**
+ * myStartupFun - executed before main function
+ *
+ * Return 0
+ */
+void myStartupFun (void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
