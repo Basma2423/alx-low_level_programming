454d2d02861dd1fc06f1d1287a7c708a7e7544bb
Basma2423 <mayarbasma2423@gmail.com>
2023-04-28 10:17:43 -0700

    s

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,5 +10,6 @@ void myStartupFun(void) __attribute__ ((constructor));
 void myStartupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my");
-	printf("house upon my back!\n");
+	printf(" house upon my back!\n");
+
 }
