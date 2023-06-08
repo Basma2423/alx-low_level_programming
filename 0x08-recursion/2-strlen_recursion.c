d73a29257648fe93655c1091e301193b9a566b8a
Basma2423 <92788812+FlouBsy@users.noreply.github.com>
2023-06-08 14:38:52 +0300

    RECURSION

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - lenght of a string
+ *
+ * @s: characters
+ *
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s != '\0')
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+	return (len);
+}
