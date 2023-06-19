2b317c1ae8cb4a4921d59b774defe80f0ed45822
Basma2423 <mayarbasma2423@gmail.com>
2023-06-19 12:52:08 -0700

    comet

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - initialize a dog
+ *
+ * @d: struct
+ * @name: 1st member
+ * @age: 2nd member
+ * @owner: 3rd member
+ *
+ * description: same
+ *
+ * return: 0
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
