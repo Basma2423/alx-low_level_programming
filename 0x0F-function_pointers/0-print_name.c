bf467fa90a22ee5f87f9633cec4f76f034c3a8e2
Basma2423 <92788812+FlouBsy@users.noreply.github.com>
2023-06-21 23:27:32 +0300

    Halley's Commet

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,20 +1,14 @@
 #include "function_pointers.h"
-
+#include <stdio.h>
 /**
- * print_name - takes a string and a function and applies the function on it
- *
- * @name: string
- *
- * @f: a function should be applied to name string
- *the function does something to the string
- *
- * This function applies the given function to each character in name st
- *
- * Return: None.
- */
+ * print_name - print name using pointer to function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing
+ **/
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == 0 || f == 0)
+	if (name == NULL || f == NULL)
 		return;
 
 	f(name);
