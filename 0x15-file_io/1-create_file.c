b6db287734ade7816fef3ca30568b4db8321d016
Basma2423 <mayarbasma2423@gmail.com>
2023-07-12 04:42:10 -0700

    s

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
  *
  * Return: 1 on success, -1 on failure
  */
-create_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd, w, len = 0;
 
@@ -22,7 +22,7 @@ create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	fd = open(filename, O_CREATE | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 
 	if (fd == -1 || w == -1)
