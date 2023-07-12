5945924dc88ba9e74f0899da83d322c3ca266537
Basma2423 <mayarbasma2423@gmail.com>
2023-07-12 04:28:56 -0700

    s

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,8 +7,8 @@
  *
  * @letters: number of letters to be read
  *
- * return:	w - actual number of bytes read and printed
- * 		0 - when reading fails or filname is NULL
+ * Return:	w - actual number of bytes read and printed
+ * 0 when reading fails or filename is NULL
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -21,7 +21,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
-	buff = malloc(sizeof(char)*letters);
+	buff = malloc(sizeof(char) * letters);
 	t = read(fd, buff, letters);
 	w = write(STDOUT_FILENO, buff, t);
 
